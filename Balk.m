0be155ec15dfab9f4dc50156e54577ca5749339e
Humaira Afrin <humaira.afrin.stockholm@gmail.com>
2025-01-28 23:16:51 +0100

    Uppgift 2 löst

--- Balk.m
+++ Balk.m
@@ -6,11 +6,13 @@ fp = @(x) -4 * exp(-x/2) * cos(3 * x) - 24 * exp(-x/2) * sin(3 * x); % derivatan
 x = 4.49; % Startgissning (måste ändras för nu divergerar funtionen wierd)
 tol = 1e-8; % Tolerans
 diffx = 1; iter = 0; maxiter = 100;
+res_newton=[];%uppgift 2c
 
 while diffx > tol && iter < maxiter % sålänge diff är större än tolerasen
     iter = iter + 1; % öka antal iterationer
     xnew = x - (f ( x ) / fp ( x )) ; % Räkna xn+1=xn-(f(x)/f'(x)) 
     diffx = abs ( xnew - x ) ; % skillnaden/längden mellan två pinkter:  | x ( n +1) -x ( n ) |
+    res_newton=[res_newton,diffx];
     x = xnew ; % Uppdatera xn med värdet av xn+1, alltså stargissningen uppdateras
     disp ([ iter xnew diffx ]) % Utskrift pa skarmen
  end
@@ -29,11 +31,13 @@ tol = 1e-8; % tolerans
 diffx = 1; % differensen
 iter = 0; % antal iterationer
 maxiter = 100;
+res_sekant=[]; % uppgift 2c
 
 while diffx > tol && iter < maxiter % sålänge differensen är större än toleransen
     iter = iter + 1; % Increment iteration 
     hnew = h1 - g(h1) * ((h1 - h0) / (g(h1) - g(h0))); % räkna xn+1 = xn-f(xn)*(x-xn)/f(xn)-f(x0) här är xn =h1 och x= h0
     diffx = abs(hnew - h1); % skillnaden/längden mellan två pinkter:  | x ( n +1) -x ( n ) |
+    res_sekant=[res_sekant,diffx];
     h0 = h1;  % uppdatera så att i andra iteration x=h1
     h1 = hnew; % uppdatera så att i andra iteration xn = xn+1 alltså den nya x värdet
     disp([iter, hnew, diffx]); 
@@ -44,44 +48,11 @@ T_sekant = h1
 
 %Uppgift 2c: Jämför konvergenshastigheten
 
-% #############  Kopiera sekants metod ################
-
-% Startgissning
-h0 = 4; 
-h1 = 5;
-diffx = []; % Difference till en vector för att lagra skillnader |h_{n+1} - h_n| för olika n ={0,1,2....}
-iter = 0;
-maxiter = 100; % Maximalt antal iterationer
-while iter < maxiter
-    iter = iter + 1; % Öka antal iterationer
-    % Beräkna nästa steg
-    hnew = h1 - g(h1) * ((h1 - h0) / (g(h1) - g(h0))); 
-    % Spara skillnaden |h_{n+1} - h_n|
-    diffx = [diffx, abs(hnew - h1)];
-    % Uppdatera för nästa iteration
-    h0 = h1;
-    h1 = hnew;
-end
-%###########  kopiera Newtons metod  ###############
-
-x = 4.49; % Startgissning
-diffx1 = []; %Difference till en vector för att lagra skillnader |h_{n+1} - h_n| för olika n ={0,1,2....}
-iter1 = 0; maxiter = 100;
-while iter1 < maxiter
-    iter1 = iter1 + 1; % increment iterations
-    xnew = x - (f ( x ) / fp ( x )) ; % räkna xn+1
-    diffx1 = [diffx1, abs( xnew - x )] ; % lägg till diff för varje iteration i vektorn
-    x = xnew ; % uppdatera startgissning
- end
-
-% ###############  Plotta konvergenshastigheten  ################
 figure;
-semilogy(1:length(diffx), diffx,'LineWidth',2);
+semilogy(1:length(res_sekant), res_sekant,'LineWidth',2);
 hold on;
-semilogy(1:length(diffx1), diffx1, 'LineWidth',2);
-
+semilogy(1:length(res_newton), res_newton, 'LineWidth',2);
 grid on;
-
 xlabel('Iterationer (n)');
 ylabel('|h_{n+1} - h_n|');
 title('Konvergenshastighet');
